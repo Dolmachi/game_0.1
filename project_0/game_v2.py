48330a19222d9e532748f784d8a18b2c70e7d775
Dolmachi <wildhare8931@mail.ru>
2023-02-26 23:00:31 +1000

    edit game_v2.py

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -46,5 +46,5 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-if __name__ == '__main':
+if __name__ == '__main__':
     score_game(random_predict)
